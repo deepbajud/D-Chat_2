4089706b25b9b2b6b9bf01acce6c55a4d3e686a5
Deep Patel <118447327+deepbajud@users.noreply.github.com>
2023-09-08 11:14:10 +0530

    Commit 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //sdp
+    implementation("com.intuit.sdp:sdp-android:1.1.0")
 }
